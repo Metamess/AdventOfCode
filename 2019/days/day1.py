f7b57469eef8c171b56190745090244cbf32ff2a
Metamess <metamess@gmail.com>
2019-12-02 20:24:29 +0100

    Refactored for a new year (2019) Added Day 1 of 2019

---
+++ 2019/days/day1.py
@@ -0,0 +1,40 @@
+import math
+
+
+def part1():
+	"""
+	To find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+	What is the sum of the fuel requirements for all of the modules on your spacecraft?
+	"""
+	res = 0
+	values = read_input()
+	for mass in values:
+		res += math.floor(mass//3) - 2
+	print(res)
+
+
+def part2():
+	"""
+	Fuel itself requires fuel just like a module.
+	Any mass that would require negative fuel should instead be treated as if it requires zero fuel;
+	for each module mass, calculate its fuel and add it to the total.
+	What is the sum of the fuel requirements for all of the modules on your spacecraft
+	when also taking into account the mass of the added fuel?
+	"""
+	res = 0
+	values = read_input()
+	for mass in values:
+		while True:
+			mass = math.floor(mass // 3) - 2
+			if mass < 0:
+				break
+			res += mass
+	print(res)
+
+
+def read_input():
+	values = []
+	with open('input/day1.txt') as input_file:
+		for line in input_file:
+			values.append(int(line))
+	return values
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Advent Of Code 2018
-My solutions to the programming challenges from https://adventofcode.com/2018
+# Advent Of Code
+My solutions to the programming challenges from https://adventofcode.com/ for 2018 and 2019
 
 All solutions are written in Python 3, and can be executed by running:
 ```
