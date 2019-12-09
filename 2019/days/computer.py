5500fe6db45d3571a2c204a5b1dcfa45dfe72da8
Metamess <metamess@gmail.com>
2019-12-09 22:58:13 +0100

    Day 5, 2019

--- 2019/days/computer.py
+++ 2019/days/computer.py
@@ -1,26 +1,146 @@
 
 """"
+
+parameter mode 0, position mode
+parameter mode 1, immediate mode
+
+Parameter modes are stored in the same value as the instruction's opcode.
+The opcode is the rightmost two digits of the first value in an instruction.
+Parameter modes are single digits, one per parameter, read right-to-left from the opcode.
+
 Opcode 1 adds together numbers read from two positions and stores the result in a third position.
-Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them.
+Opcode 2 multiplies together numbers read from two positions and stores the result in a third position.
+Opcode 3 takes a single integer as input and saves it to the position given by its only parameter. For example, the instruction 3,50 would take an input value and store it at address 50.
+Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the value at address 50.
+Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
 Opcode 99 means that the program is finished and should immediately halt.
 """
 
+param_count = [0, 3, 3, 1, 1, 2, 2, 3, 3]
 
-def run_program(program):
+
+def run_program(program, input_value=0):
 	i = 0
 	while True:
-		opcode = program[i]
+		instruction = str(program[i])
+		# print('instruction: ' + instruction, i)
+		if len(instruction) == 1:
+			opcode = program[i]
+			modes = '0' * param_count[opcode]
+		else:
+			opcode = int(str(instruction)[-2:])
+			if opcode is 99:
+				break
+			# print(opcode)
+			modes = str(instruction)[:-2]
+			modes = modes[::-1] + '0' * (param_count[opcode] - len(modes))
 		if opcode is 99:
 			break
-		p1 = program[i + 1]
-		p2 = program[i + 2]
-		p3 = program[i + 3]
 
-		if opcode is 1:
-			program[p3] = program[p1] + program[p2]
-		elif opcode is 2:
-			program[p3] = program[p1] * program[p2]
+		if opcode is 1:  # Addition
+			p1 = program[i + 1]
+			if modes[0] == '0':
+				p1 = program[p1]
+
+			p2 = program[i + 2]
+			if modes[1] == '0':
+				p2 = program[p2]
+
+			p3 = program[i + 3]
+			if modes[2] == '1':
+				raise ValueError()
+
+			program[p3] = p1 + p2
+
+		elif opcode is 2:  # Multiplication
+			p1 = program[i + 1]
+			if modes[0] == '0':
+				p1 = program[p1]
+
+			p2 = program[i + 2]
+			if modes[1] == '0':
+				p2 = program[p2]
+
+			p3 = program[i + 3]
+			if modes[2] == '1':
+				raise ValueError()
+			program[p3] = p1 * p2
+
+		elif opcode is 3:  # Set
+			p1 = program[i + 1]
+			if modes[0] == '1':
+				raise ValueError()
+			program[p1] = input_value
+
+		elif opcode is 4:  # Get
+			p1 = program[i + 1]
+			if modes[0] == '0':
+				p1 = program[p1]
+			print(p1)
+
+		elif opcode is 5:  # Jump-if-true
+			p1 = program[i + 1]
+			if modes[0] == '0':
+				p1 = program[p1]
+
+			p2 = program[i + 2]
+			if modes[1] == '0':
+				p2 = program[p2]
+
+			if p1 != 0:
+				i = p2
+				continue
+		elif opcode is 6:  # Jump-if-false
+			p1 = program[i + 1]
+			if modes[0] == '0':
+				p1 = program[p1]
+
+			p2 = program[i + 2]
+			if modes[1] == '0':
+				p2 = program[p2]
+
+			if p1 == 0:
+				i = p2
+				continue
+
+		elif opcode is 7:  # Less than
+			p1 = program[i + 1]
+			if modes[0] == '0':
+				p1 = program[p1]
+
+			p2 = program[i + 2]
+			if modes[1] == '0':
+				p2 = program[p2]
+
+			p3 = program[i + 3]
+			if modes[2] == '1':
+				raise ValueError()
+
+			if p1 < p2:
+				program[p3] = 1
+			else:
+				program[p3] = 0
+		elif opcode is 8:  # Equals
+			p1 = program[i + 1]
+			if modes[0] == '0':
+				p1 = program[p1]
+
+			p2 = program[i + 2]
+			if modes[1] == '0':
+				p2 = program[p2]
+
+			p3 = program[i + 3]
+			if modes[2] == '1':
+				raise ValueError()
+
+			if p1 == p2:
+				program[p3] = 1
+			else:
+				program[p3] = 0
 		else:
 			raise ValueError("Unexpected opcode: " + str(opcode))
-		i += 4
+		i += param_count[opcode] + 1
 	return program
---
+++ 2019/days/day5.py
@@ -0,0 +1,22 @@
+from . import computer
+
+
+def part1():
+	"""
+	After providing 1 to the only input instruction and passing all the tests, what diagnostic code does the program produce?
+	"""
+	program = read_input()
+	computer.run_program(program, 1)
+
+
+def part2():
+	"""
+	What is the diagnostic code for system ID 5?
+	"""
+	program = read_input()
+	computer.run_program(program, 5)
+
+
+def read_input():
+	with open('input/day5.txt') as input_file:
+		return [int(x) for x in input_file.readline().split(',')]
