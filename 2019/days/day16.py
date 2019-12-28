8c22867fe81476c182e0211df925713d4a7eb820
Metamess <matthijsamesz@hotmail.com>
2019-12-28 15:51:13 +0100

    Day 16 Part 2, 2019

--- 2019/days/day16.py
+++ 2019/days/day16.py
@@ -29,9 +29,28 @@ def part1():
 
 def part2():
     """
-
+    The real signal is your puzzle input repeated 10000 times.
+    The first seven digits of your initial input signal also represent the message offset.
+    The message offset is the location of the eight-digit message in the final output list.
+    After repeating your input signal 10000 times and running 100 phases of FFT, what is the eight-digit message embedded in the final output list?
     """
-    pass
+    signal = read_input()
+    repeat = 10000
+    offset = int(''.join(str(d) for d in signal[:7]))
+    assert offset > len(signal)*repeat // 2
+    # The pattern forms an upper-triangular matrix, the bottom half of which is merely 1's
+    # Because of this and the offset, we need only bother with digits after the offset amount
+    # Since the offset is larger than half the input length, we only ever sum
+    length_left = len(signal)*repeat - offset
+    inverse_signal = (signal[::-1] * (length_left // len(signal) + 1))[:length_left]
+    for fft in range(100):
+        next_signal = []
+        running_sum = 0
+        for d in inverse_signal:
+            running_sum += d
+            next_signal.append(running_sum % 10)
+        inverse_signal = next_signal
+    print(''.join(str(d) for d in inverse_signal[::-1][:8]))
 
 
 def read_input():
